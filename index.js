e40970a9bdfe6980a3fd0652e6a115359ef148d2
aquamark-io <heyaquamark@gmail.com>
2025-05-12 21:38:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -138,8 +138,8 @@ app.post("/watermark", async (req, res) => {
     const logoWidth = width * 0.2;
     const logoHeight = (logoWidth / watermarkImage.width) * watermarkImage.height;
 
-    for (let x = 0; x < width; x += (logoWidth + 100)) {
-      for (let y = 0; y < height; y += (logoHeight + 100)) {
+    for (let x = 0; x < width; x += (logoWidth + 150)) {
+      for (let y = 0; y < height; y += (logoHeight + 150)) {
         watermarkPage.drawImage(watermarkImage, {
           x,
           y,
